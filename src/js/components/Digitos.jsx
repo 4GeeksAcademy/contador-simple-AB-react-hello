ee8fd727f7a67e58c8ffd6b37f99a7b11a2c697d
andersontbernal <andersonbernal7@gmail.com>
2025-07-09 11:03:44 +0000

    proyecto contador en curso

---
+++ src/js/components/Digitos.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Digitos = ({number}) => <div>{number}</div>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import { Digitos } from "./Digitos.jsx";
 //create your first component
 const Home = () => {
+
+	const [timer, SetTimer] = useState(0)
+	
+	useEffect(()=>{},[])
+	
+	setTimeout(()=>{
+		SetTimer(value=> value+1)	
+
+	}, 1000)
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Digitos number={<span className="fa fa-clock"></span>}/>
+			<Digitos number={timer}/>
+			<Digitos number={timer}/>
+			<Digitos number={timer}/>
+			<Digitos number={timer}/>
+			<Digitos number={Math.floor((timer/10)%10)}/>
+			<Digitos number={Math.floor(timer%10)}/>			
 		</div>
 	);
 };
+
+
+ 
 
 export default Home;
